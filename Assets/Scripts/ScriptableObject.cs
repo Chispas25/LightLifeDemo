a513bdfebe8baff18159ad6afc3e9738aaf6875d
Patata-crypto <mariacastillam4@gmail.com>
2025-05-26 23:21:19 +0200

    Puertas que se abren con llave hechas

--- Assets/Scripts/ScriptableObject.cs
+++ Assets/Scripts/ScriptableObject.cs
@@ -3,11 +3,10 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "NewKeyItem", menuName = "Inventory/Key Item")]
 public class KeyItem : InventoryItem
 {
-    public string doorID; // ID que debe coincidir con la puerta
+    public string doorID;
 
     public override void Use(GameObject user)
     {
-        // El comportamiento se maneja en el script de la puerta
         Debug.Log("Este ítem debe usarse cerca de una puerta con ID: " + doorID);
     }
 }
